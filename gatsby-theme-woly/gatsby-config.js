4d03d2cf90180e0de7ac141b5361a83228b585e9
Sergey Sova <mail@sergeysova.com>
2020-08-18 18:28:48 +0300

    add mdx

--- gatsby-theme-woly/gatsby-config.js
+++ gatsby-theme-woly/gatsby-config.js
@@ -1,3 +1,16 @@
-console.log("2")
+module.exports = (themeOptions) => {
+  const IS_DEV = process.env.NODE_ENV !== 'production';
+  console.log('OPTIONS', themeOptions);
 
-module.exports = {}
+  return {
+    plugins: [
+      'gatsby-plugin-sitemap',
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: 'gatsby-plugin-styled-components',
+        options: { displayName: IS_DEV },
+      },
+      { resolve: `gatsby-plugin-mdx` },
+    ],
+  };
+};
