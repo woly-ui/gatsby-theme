8a0d14b014814cb50b78c333913d2a5ab568380e
Ainur <33234903+purplecandle@users.noreply.github.com>
2021-06-23 15:28:18 +0300

    feat: add tab navigation in component pages (#17)
    * feat: add tab navigation in component pages
    
    * refactor: change tabs' styles

--- gatsby-theme-woly/src/templates/usage.js
+++ gatsby-theme-woly/src/templates/usage.js
@@ -1,41 +1,57 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { pascalCase } from 'change-case';
 import { Layout } from '../components/layout';
+import { Tabs } from '../components/tabs';
 
-const installation = ({ package: p }) =>
-  `npm install ${p}
-# or
-yarn add ${p}`;
+import { pageSections } from '../lib/constants';
+
+const getTabData = ({ pages, mapper }) => {
+  return mapper.reduce((all, { code, label, renderHeader }) => {
+    const page = pages.find((page) => page.meta.type === code);
+
+    if (page) {
+      all.push({
+        label,
+        meta: page.meta,
+        content: <MDXRenderer>{page.body}</MDXRenderer>,
+        header: renderHeader,
+      });
+    }
+
+    return all;
+  }, []);
+};
 
 const ComponentPage = ({ data, pageContext }) => {
-  const { body } = data.usage;
-  const { name, category, package: packageName } = pageContext;
+  const pages = data.allMdx.nodes.map(({ id, body }) => {
+    const meta = pageContext.pages[id];
+
+    return {
+      meta,
+      body,
+    };
+  });
+
+  const tabData = getTabData({ pages, mapper: pageSections });
 
   return (
     <Layout>
       <div>
-        <h2>{name}</h2>
-        <pre>
-          import {'{'} {pascalCase(name)} {'}'} from "{packageName}";
-        </pre>
-        {/* <h3>Installation</h3> */}
-        {/* prettier-ignore */}
-        {/* <pre>
-          {installation(frontmatter)}
-        </pre> */}
-        <MDXRenderer>{body}</MDXRenderer>
+        <h2>{pageContext.name}</h2>
+        <Tabs data={tabData} />
       </div>
     </Layout>
   );
 };
 
 export const pageQuery = graphql`
-  query($pageID: String!) {
-    usage: mdx(id: { eq: $pageID }) {
-      fileAbsolutePath
-      body
+  query($ids: [String]) {
+    allMdx(filter: { id: { in: $ids } }) {
+      nodes {
+        id
+        body
+      }
     }
   }
 `;
